ec41a3749cc5e729c279e41350d31dfe2b60b782
skyriderdragoon <92641974+skyriderdragoon@users.noreply.github.com>
2026-06-18 20:14:30 +0100

    adjustments to admin list and added new rule

--- src/assets/staff/index.ts
+++ src/assets/staff/index.ts
@@ -1,8 +1,5 @@
-import orion from './orion.avif';
-import reggie from './reggie.avif';
 import snipsel from './snipsel.avif';
 import toob from './toob.avif';
-import xeechu from './xeechu.avif';
 import placeholder from './placeholder.svg';
 import sky from './sky.avif';
 import daisy from './daisy.png'
@@ -10,18 +7,19 @@ import socks from './socks.png'
 import edison from './edison.avif'
 import felder from './felder.avif'
 import harlequeen from './harlequeen.avif'
+import bunjie from "./bunjie.jpg"
+import happybara from "./happybara.png"
 
 export {
-    orion,
-    reggie,
     snipsel,
     toob,
-    xeechu,
     placeholder,
     sky,
     socks,
     daisy,
     edison,
     harlequeen,
-    felder
+    felder,
+    bunjie,
+    happybara
 }
